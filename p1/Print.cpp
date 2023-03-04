283e21710d1a9dea6e0d2b2cdec2b7f58506eb61
hjc2000 <hjc.666@foxmail.com>
2023-03-04 23:27:45 +0800

    第一次提交

---
+++ p1/Print.cpp
@@ -0,0 +1,8 @@
+#include "Print.h"
+#include <iostream>
+
+void Print(void)
+{
+    using namespace std;
+    cout << "hello world" << endl;
+}
---
+++ p1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Print.h"
+
+using namespace std;
+int main(void)
+{
+    Print();
+    return 0;
+}
